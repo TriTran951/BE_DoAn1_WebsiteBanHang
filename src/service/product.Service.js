f5c12ac82b18b11db08ca1a80571b1ec3643b2d7
20520143 <92258237+20520143@users.noreply.github.com>
2023-05-10 12:59:30 +0700

    update api

--- src/service/product.Service.js
+++ src/service/product.Service.js
@@ -2,56 +2,68 @@ import SanPhamModel from '../model/sanpham.model.js';
 import LoaiSanPhamModel from '../model/loaisanpham.model.js';
 
 import mongoose from 'mongoose';
-let getHomeProduct = async () => {
-    let Product = await SanPhamModel.find();
-    let homeProduct = [];
-    let phone = [];
-    let o = 0;
-    let DienThoai = '644dd1bc94aaffdfaed2fe3c';
-    for (let i = 0; i < Product.length; i++) {
-        if (Product[i].MaLoaiSanPham == DienThoai) {
-            phone[o++] = Product[i];
-        }
-    }
-    homeProduct[0] = phone;
-    return homeProduct;
-};
 
 let getSmartphone = async () => {
-    let phone = await SanPhamModel.find({
-        MaLoaiSanPham: '644dd1bc94aaffdfaed2fe3c',
-    });
+    let phone = await SanPhamModel.find(
+        {
+            MaLoaiSanPham: '644dd1bc94aaffdfaed2fe3c',
+        },
+        '-SanPhamLienQuan ',
+    );
     return phone;
 };
 let getLaptop = async () => {
-    let laptop = await SanPhamModel.find({
-        MaLoaiSanPham: '644dd1bb94aaffdfaed2fe39',
-    });
+    let laptop = await SanPhamModel.find(
+        {
+            MaLoaiSanPham: '644dd1bb94aaffdfaed2fe39',
+        },
+        '-SanPhamLienQuan ',
+    );
     return laptop;
 };
 let getWatch = async () => {
-    let watch = await SanPhamModel.find({
-        MaLoaiSanPham: '644e90c96b5538959065ad6c',
-    });
+    let watch = await SanPhamModel.find(
+        {
+            MaLoaiSanPham: '644e90c96b5538959065ad6c',
+        },
+        '-SanPhamLienQuan ',
+    );
     return watch;
 };
 let getTablet = async () => {
-    let tablet = await SanPhamModel.find({
-        MaLoaiSanPham: '644f56cbb2cd385ebfffb7ed',
-    });
+    let tablet = await SanPhamModel.find(
+        {
+            MaLoaiSanPham: '644f56cbb2cd385ebfffb7ed',
+        },
+        '-SanPhamLienQuan ',
+    );
     return tablet;
 };
 let getEarphone = async () => {
-    let earphone = await SanPhamModel.find({
-        MaLoaiSanPham: '644f5733f1488433820db285',
-    });
+    let earphone = await SanPhamModel.find(
+        {
+            MaLoaiSanPham: '644f5733f1488433820db285',
+        },
+        '-SanPhamLienQuan ',
+    );
     return earphone;
 };
+let test = async () => {
+    let test = await SanPhamModel.find({
+        MaLoaiSanPham: '644dd1bc94aaffdfaed2fe3c',
+    })
+        .populate({
+            path: 'thuonghieu',
+            select: 'TenThuongHieu',
+        })
+        .distinct('MaThuongHieu');
+    return test;
+};
 export default {
-    getHomeProduct,
     getSmartphone,
     getEarphone,
     getLaptop,
     getTablet,
     getWatch,
+    test,
 };
