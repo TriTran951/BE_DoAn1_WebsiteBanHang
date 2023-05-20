d3091d59940a8cc553eca8ad62f9802a9014433d
TriTran951 <90551445+TriTran951@users.noreply.github.com>
2023-05-20 21:02:25 +0700

    fix cors connect FE

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import cors from 'cors';
 import cookie from 'cookie-parser';
 import Router from './router/index.js';
 import dotenv from 'dotenv';
@@ -22,12 +21,25 @@ const bootServer = () => {
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
 
-    app.use(
-        cors({
-            origin: [process.env.hostFE],
-            optionsSuccessStatus: 200,
-        }),
-    );
+    // Add headers before the routes are defined
+    app.use(function (req, res, next) {
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', process.env.hostFE);
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+
+        // Pass to next layer of middleware
+        next();
+    });
+
     app.use(cookie());
     //router api
     app.use('/api', Router);
